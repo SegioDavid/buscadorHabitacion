06d6b497ac757c20b45df6b680c36413b4780061
sergiogalan99 <sergiocalamonte2018@gmail.com>
2019-11-04 21:05:46 +0100

    Cambios

--- src/app/core/model/generador-hoteles.ts
+++ src/app/core/model/generador-hoteles.ts
@@ -1,8 +1,22 @@
+
+import { Categoria } from './categoria';
 import { Hotel } from './hotel';
 export class GeneradorHoteles {
-    private nombreHoteles = [""];
+   private _arrayNombreHoteles = []
+   private _habitaciones =[]
 
     public getHoteles(): Hotel[] {
-        return null;
+        this._arrayNombreHoteles.push(new Hotel("Hotel Parissi",Categoria["⭐⭐⭐⭐⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Realeza",Categoria["⭐⭐⭐⭐⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Mulan",Categoria["⭐⭐⭐⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Platinum",Categoria["⭐⭐⭐⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Bjusjine",Categoria["⭐⭐⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Wine",Categoria["⭐⭐⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Caravela",Categoria["⭐⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Sergio",Categoria["⭐⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Ashera",Categoria["⭐"],this._habitaciones));
+        this._arrayNombreHoteles.push(new Hotel("Hotel Dominos",Categoria["⭐"],this._habitaciones));
+        return this._arrayNombreHoteles;
     }
 }
+    
--- src/app/resultado/resultado.component.ts
+++ src/app/resultado/resultado.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ResultadoComponent implements OnInit {
 
   
+   
   constructor() {
     
   }
