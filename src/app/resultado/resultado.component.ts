b35625ebecff831ad4b8001e2ffa3f2ae4c57915
dariasv1108 <dariasv01@gmail.com>
2019-11-05 17:29:53 +0100

    Vistas provisional

--- src/app/resultado/resultado.component.ts
+++ src/app/resultado/resultado.component.ts
@@ -1,5 +1,9 @@
 import { PeticionControllerService } from './../shared/peticion-controller.service';
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../core/model/categoria';
+import { Camas } from '../core/model/camas';
+import { Capacidad } from '../core/model/capacidad';
+import { Extras } from '../core/model/extras';
 
 
 @Component({
@@ -8,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resultado.component.scss']
 })
 export class ResultadoComponent implements OnInit {
-
+  private _arrayCategoria = [];
+  private _arrayCama = [];
+  private _arrayCapacidad = [];
+  private _arrayExtras = [];
+  
   constructor(public resultadoService: PeticionControllerService) {
-   
+    for (let index = 0; index < Object.keys(Categoria).length / 2; index++) {
+      this._arrayCategoria.push(Categoria[index].toString());
+    }
+    for (let index = 0; index < Object.keys(Camas).length / 2; index++) {
+      this._arrayCama.push(Camas[index].toString());
+    }
+    for (let index = 0; index < Object.keys(Capacidad).length / 2; index++) {
+      this._arrayCapacidad.push(Capacidad[index].toString());
+    }
+    for (let index = 0; index < Object.keys(Extras).length / 2; index++) {
+      this._arrayExtras.push(Extras[index].toString());
+    }
   }
   ngOnInit() {
   }
